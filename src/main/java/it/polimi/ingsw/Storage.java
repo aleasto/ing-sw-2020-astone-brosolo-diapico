b458ffae0486f5c8902aff1dce3ddb8f69ef3557
Alessandro <ales.astone@gmail.com>
2020-03-30 17:16:20 +0200

    small refractoring of Storage
     * Levels go from 0 to 3, not 1 to 4
     * Handle out of bounds

--- src/main/java/it/polimi/ingsw/Storage.java
+++ src/main/java/it/polimi/ingsw/Storage.java
@@ -1,12 +1,12 @@
 package it.polimi.ingsw;
 
 public class Storage {
-    private static final int MAX_LVL1 = 22;
-    private static final int MAX_LVL2 = 18;
-    private static final int MAX_LVL3 = 14;
+    private static final int MAX_LVL0 = 22;
+    private static final int MAX_LVL1 = 18;
+    private static final int MAX_LVL2 = 14;
     private static final int MAX_DOME = 14;
 
-    private int pieceAmt[] = new int[] { MAX_LVL1, MAX_LVL2, MAX_LVL3, MAX_DOME };
+    private int pieceAmt[] = new int[] { MAX_LVL0, MAX_LVL1, MAX_LVL2, MAX_DOME };
 
     /**
      * Retrieve a piece from the storage
@@ -14,8 +14,11 @@ public class Storage {
      * @return false if no available piece of the given type, true otherwise
      */
     public boolean retrieve(int piece) {
-        if (pieceAmt[piece - 1] > 0) {
-            pieceAmt[piece - 1]--;
+        if (piece > pieceAmt.length - 1 || piece < 0)
+            return false;
+
+        if (pieceAmt[piece] > 0) {
+            pieceAmt[piece]--;
             return true;
         }
         return false;
@@ -27,6 +30,9 @@ public class Storage {
      * @return the number of available pieces of `piece`
      */
     public int getAvailable(int piece) {
-        return pieceAmt[piece - 1];
+        if (piece > pieceAmt.length - 1 || piece < 0)
+            return 0;
+
+        return pieceAmt[piece];
     }
 }
