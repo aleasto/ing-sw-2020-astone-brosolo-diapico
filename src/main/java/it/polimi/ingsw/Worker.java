e04ddde4e28032db270cc85199922cda0d297ed7
Alessandro Astone <ales.astone@gmail.com>
2020-03-31 00:18:09 +0200

    add unit tests for BaseActions
     * Also add the needed interfaces for other classes referenced here.

--- src/main/java/it/polimi/ingsw/Worker.java
+++ src/main/java/it/polimi/ingsw/Worker.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw;
 public class Worker {
     private Tile tile;
 
+    public Worker(Tile startingTile) {
+        tile = startingTile;
+    }
+
     public Tile getTile() {
         return tile;
     }
