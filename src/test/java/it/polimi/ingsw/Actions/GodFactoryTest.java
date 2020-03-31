a3695dd7e766033365627ae564226c2a3f42d155
Alessandro Astone <ales.astone@gmail.com>
2020-03-31 16:38:52 +0200

    introduce the god factory
     * Takes gods.json as a config file
     * Decorate BaseActions with the specified "self" decorators
     * Decorate others with the specified "enemies" decorators

---
+++ src/test/java/it/polimi/ingsw/Actions/GodFactoryTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.Actions;
+
+import it.polimi.ingsw.Actions.Decorators.CanMoveTwice;
+import it.polimi.ingsw.Actions.Decorators.CanSwapWithEnemy;
+import it.polimi.ingsw.Actions.Decorators.CannotMoveUpIfEnemyDid;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GodFactoryTest {
+
+    @Test
+    void makeActions() {
+        String customJson = "{" +
+                "'Foo': { 'self': [CanMoveTwice], 'enemies': [] }, " +
+                "'Bar': { 'self': [CanSwapWithEnemy], 'enemies': [] }, " +
+                "'Baz': { 'self': [], 'enemies': [] }, " +
+                "'Qux': { 'self': [], 'enemies': [CannotMoveUpIfEnemyDid] }" +
+                "}";
+        GodFactory.loadJson(new ByteArrayInputStream(customJson.getBytes()));
+
+        Map<String, Actions> actionsMap = GodFactory.makeActions(Arrays.asList("Foo", "Bar", "Baz"));
+        assertEquals(CanMoveTwice.class, actionsMap.get("Foo").getClass());
+        assertEquals(CanSwapWithEnemy.class, actionsMap.get("Bar").getClass());
+        assertEquals(BaseActions.class, actionsMap.get("Baz").getClass());
+
+        actionsMap = GodFactory.makeActions(Arrays.asList("Qux", "Baz"));
+        assertEquals(BaseActions.class, actionsMap.get("Qux").getClass());
+        assertEquals(CannotMoveUpIfEnemyDid.class, actionsMap.get("Baz").getClass());
+    }
+}
