5ea273a1834f8bf42e7d0594676aceb2479bac6c
GiacoBros <giacomogiulio.brosolo@mail.polimi.it>
2020-04-14 16:33:09 +0200

    classe Player

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.Actions.Actions;
+
 public class Player {
+    private String name;
+    private String godName;
+    private Actions actions;
+    private int godLikeLvl;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGodName() {
+        return godName;
+    }
+
+    public int getGodLikeLvl() {
+        return godLikeLvl;
+    }
+
+    public Actions getActions() {
+        return actions;
+    }
+
+    public void setActions(Actions actions) {
+        this.actions = actions;
+    }
 }
