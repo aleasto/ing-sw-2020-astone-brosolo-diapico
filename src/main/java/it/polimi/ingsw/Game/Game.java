87a228ce603f8789b3b87278a8abe2fdbcb7e5b7
Alessandro <ales.astone@gmail.com>
2020-04-18 22:32:30 +0200

    better command handling

--- src/main/java/it/polimi/ingsw/Game/Game.java
+++ src/main/java/it/polimi/ingsw/Game/Game.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Game;
 
 import it.polimi.ingsw.Exceptions.InvalidBuildActionException;
+import it.polimi.ingsw.Exceptions.InvalidCommandException;
 import it.polimi.ingsw.Exceptions.InvalidMoveActionException;
 import it.polimi.ingsw.Game.Actions.Actions;
 import it.polimi.ingsw.Game.Actions.GodFactory;
@@ -9,7 +10,6 @@ import it.polimi.ingsw.View.CommandMessage;
 import it.polimi.ingsw.View.Observable;
 import it.polimi.ingsw.View.Observer;
 
-import java.security.InvalidParameterException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,11 +54,10 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
      */
     public void Move(int fromX, int fromY, int toX, int toY) {
         Player p = players.get(currentPlayer);
-        Pair<Worker, Tile> action = parseAction(fromX, fromY, toX, toY);
-        Worker w = action.getFirst();
-        Tile to = action.getSecond();
-
         try {
+            Pair<Worker, Tile> action = parseAction(fromX, fromY, toX, toY);
+            Worker w = action.getFirst();
+            Tile to = action.getSecond();
             if (p.getActions().canMove() && p.getActions().validMove(w, to)) {
                 p.getActions().doMove(w, to);
             } else {
@@ -68,7 +67,8 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
                 }
                 throw new InvalidMoveActionException(errorMessage);
             }
-        } catch (InvalidParameterException | InvalidMoveActionException ex) {
+            notifyChange("Ok!");
+        } catch (InvalidCommandException | InvalidMoveActionException ex) {
             notifyChange(ex.getMessage());
         }
     }
@@ -82,10 +82,10 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
      */
     public void Build(int fromX, int fromY, int toX, int toY, int lvl) {
         Player p = players.get(currentPlayer);
-        Pair<Worker, Tile> action = parseAction(fromX, fromY, toX, toY);
-        Worker w = action.getFirst();
-        Tile to = action.getSecond();
         try {
+            Pair<Worker, Tile> action = parseAction(fromX, fromY, toX, toY);
+            Worker w = action.getFirst();
+            Tile to = action.getSecond();
             if (p.getActions().canBuild() && p.getActions().validBuild(w, to, lvl) && storage.getAvailable(lvl) > 0) {
                 storage.retrieve(lvl);
                 p.getActions().doBuild(w, to, lvl);
@@ -96,7 +96,8 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
                 }
                 throw new InvalidBuildActionException(errorMessage);
             }
-        } catch (InvalidParameterException | InvalidBuildActionException ex) {
+            notifyChange("Ok!");
+        } catch (InvalidCommandException | InvalidBuildActionException ex) {
             notifyChange(ex.getMessage());
         }
     }
@@ -109,17 +110,17 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
         players.get(currentPlayer).getActions().beginTurn();
     }
 
-    private Pair<Worker, Tile> parseAction(int fromX, int fromY, int toX, int toY) throws InvalidParameterException {
+    private Pair<Worker, Tile> parseAction(int fromX, int fromY, int toX, int toY) throws InvalidCommandException {
         Tile from, to;
         try {
             from = board.getAt(fromX, fromY);
             to = board.getAt(toX, toY);
         } catch(IndexOutOfBoundsException ex) {
-            throw new InvalidParameterException(ex.getMessage());
+            throw new InvalidCommandException("The specified tile(s) do not exist");
         }
         Worker w = from.getOccupant();
         if (from.getOccupant() == null) {
-            throw new InvalidParameterException("The specified starting tile is not hosting a worker");
+            throw new InvalidCommandException("The specified starting tile is not hosting a worker");
         }
 
         return new Pair(w, to);
@@ -133,10 +134,10 @@ public class Game extends Observable<String> implements Observer<CommandMessage>
         }
 
         switch (message.getAction()) {
-            case CommandMessage.MOVE:
+            case MOVE:
                 Move(message.getFromX(), message.getFromY(), message.getToX(), message.getToY());
                 break;
-            case CommandMessage.BUILD:
+            case BUILD:
                 Build(message.getFromX(), message.getFromY(), message.getToX(), message.getToY(), message.getToZ());
                 break;
         }
