6fb619e5714fb0c6c604f0b9fad9d704db665e37
Alessandro <ales.astone@gmail.com>
2020-04-20 14:26:45 +0200

    verify that we cannot headbutt onto a dome or outside of the board

--- src/main/java/it/polimi/ingsw/Game/Actions/Decorators/CanSwapWithEnemy.java
+++ src/main/java/it/polimi/ingsw/Game/Actions/Decorators/CanSwapWithEnemy.java
@@ -17,11 +17,6 @@ public class CanSwapWithEnemy extends ActionsDecorator {
         // If the destination tile is occupied by an enemy, pretend that is empty instead and ask super
         // by constructing a fake tile object.
         if (!to.isEmpty() && !w.getOwner().equals(to.getOccupant().getOwner())) {
-            // Since we will pass in a fake tile, we can't rely on super to check if src and dst are different.
-            // Actually now we can, Tile::equals checks for (x, y)
-            // if (to == w.getTile())
-            //    return false;
-
             Tile fake = new Tile(to.getBoard(), to.getX(), to.getY());
             for (int i = 0; i < to.getHeight(); i++)
                 fake.buildUp();
