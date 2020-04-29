58eb885a26f3fb08e293745a8dedac7ba99f9e54
AndreaDIapico <10619706@polimi.it>
2020-04-29 17:28:08 +0200

    Final two Base Decorators

---
+++ src/test/java/it/polimi/ingsw/Game/Actions/Decorators/CanWinByDescendingTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.Game.Actions.Decorators;
+
+import it.polimi.ingsw.Game.Actions.Actions;
+import it.polimi.ingsw.Game.Actions.BaseActions;
+import it.polimi.ingsw.Game.Board;
+import it.polimi.ingsw.Game.Player;
+import it.polimi.ingsw.Game.Tile;
+import it.polimi.ingsw.Game.Worker;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CanWinByDescendingTest {
+
+    @Test
+    void winWhenDescending() {
+        Actions myAction = new BaseActions();
+        myAction = new CanWinByDescending(myAction);
+
+        Board board = new Board();
+        Tile startingTile = board.getAt(2, 2);
+        Tile destinationTile = board.getAt(3, 3);
+        Tile badDestTile = board.getAt(3, 2);
+        startingTile.buildUp();
+        startingTile.buildUp();
+        badDestTile.buildUp();
+        badDestTile.buildUp();
+        Worker w = new Worker(new Player(), startingTile);
+
+        assertTrue(myAction.doMove(w, destinationTile));
+        destinationTile.buildUp();
+        destinationTile.buildUp();
+        assertFalse(myAction.doMove(w, badDestTile));
+    }
+}
