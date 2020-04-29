843b8b3fd143e5681a94201bde08062b51a4eec2
Alessandro Astone <ales.astone@gmail.com>
2020-04-29 20:00:00 +0200

    fixup CanBuildBeforeMove and tests

--- src/main/java/it/polimi/ingsw/Game/Actions/Decorators/CanBuildBeforeMove.java
+++ src/main/java/it/polimi/ingsw/Game/Actions/Decorators/CanBuildBeforeMove.java
@@ -6,8 +6,9 @@ import it.polimi.ingsw.Game.Tile;
 import it.polimi.ingsw.Game.Worker;
 
 public class CanBuildBeforeMove extends ActionsDecorator {
-    private boolean HasBuiltBeforeMoving = false;
-    private boolean HasMoved = false;
+    private boolean hasBuiltBeforeMoving = false;
+    private boolean hasBuiltAfter = false;
+    private boolean hasMoved = false;
 
     public CanBuildBeforeMove (Actions decorated) {
         super(decorated);
@@ -15,18 +16,20 @@ public class CanBuildBeforeMove extends ActionsDecorator {
 
     @Override
     public void beginTurn() {
-        HasBuiltBeforeMoving  = false;
-        HasMoved = false;
+        hasBuiltBeforeMoving  = false;
+        hasBuiltAfter = false;
+        hasMoved = false;
 
         super.beginTurn();
     }
 
     @Override
     public boolean canBuild() {
-        if (HasMoved) {
+        if (!hasBuiltBeforeMoving && hasMoved) {
+            // This is a normal sequence of actions, so ask super
             return super.canBuild();
         }
-        if (!HasBuiltBeforeMoving) {
+        if (hasBuiltBeforeMoving && hasMoved && !hasBuiltAfter) {
             return true;
         }
         return false;
@@ -34,26 +37,27 @@ public class CanBuildBeforeMove extends ActionsDecorator {
 
     @Override
     public boolean doMove(Worker w, Tile to) {
-        HasMoved = true;
+        hasMoved = true;
         return super.doMove(w, to);
     }
 
     @Override
     public void doBuild(Worker w, Tile to, int level) {
-        if(!HasMoved) {
-            HasBuiltBeforeMoving = true;
+        if (!hasMoved) {
+            hasBuiltBeforeMoving = true;
+        } else {
+            hasBuiltAfter = true;
         }
         super.doBuild(w, to, level);
     }
 
     @Override
     public boolean validMove(Worker w, Tile to) {
-        if(HasBuiltBeforeMoving){
+        if (hasBuiltBeforeMoving){
             Tile from = w.getTile();
             if(to.getHeight() > from.getHeight()) {
                 return false;
             }
-
         }
         return super.validMove(w, to);
     }
