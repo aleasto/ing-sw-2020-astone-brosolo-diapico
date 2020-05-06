797409ff4beb4ed7f272403fb7a8883c166b6342
Alessandro Astone <ales.astone@gmail.com>
2020-05-07 00:18:37 +0200

    send network packets in a separate thread
    To do that:
     * Make a queue of outgoing messages
     * Factor out a RemoteView class to reuse 80% of the code

--- src/main/java/it/polimi/ingsw/Client/Client.java
+++ src/main/java/it/polimi/ingsw/Client/Client.java
@@ -19,13 +19,14 @@ public class Client {
         try {
             socket = new Socket("localhost", Server.PORT_NUMBER);
 
+            out = new ObjectOutputStream(socket.getOutputStream());
+            out.writeObject(new ConnectionMessage(player, "la lobby più bella"));
+
             CLIView cli = new CLIView(player);
             ClientRemoteView remoteView = new ClientRemoteView(socket, cli);
             new Thread(cli).start();
             new Thread(remoteView).start();
 
-            out = new ObjectOutputStream(socket.getOutputStream());
-            out.writeObject(new ConnectionMessage(player, "la lobby più bella"));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/it/polimi/ingsw/View/ClientRemoteView.java
+++ src/main/java/it/polimi/ingsw/View/ClientRemoteView.java
@@ -2,36 +2,18 @@ package it.polimi.ingsw.View;
 
 import it.polimi.ingsw.View.Communication.*;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ClientRemoteView extends View implements Runnable {
+public class ClientRemoteView extends RemoteView {
     View wrapper;
-    Socket clientSocket;
-    ObjectOutputStream out;
-    ObjectInputStream in;
 
     public ClientRemoteView(Socket clientSocket, View wrapper) {
-        super(wrapper.getPlayer());
+        super(clientSocket, wrapper.getPlayer());
         this.wrapper = wrapper;
-        this.clientSocket = clientSocket;
 
-        wrapper.addMoveCommandListener(this::sendMessageViaSocket);
-        wrapper.addBuildCommandListener(this::sendMessageViaSocket);
-        wrapper.addEndTurnCommandListener(this::sendMessageViaSocket);
-    }
-
-    private void sendMessageViaSocket(Message message) {
-        try {
-            if (out == null)
-                out = new ObjectOutputStream(clientSocket.getOutputStream());
-            out.reset(); // Clear cache, otherwise mutable objects are not serialized again
-            out.writeObject(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        wrapper.addMoveCommandListener(this::sendRemoteMessage);
+        wrapper.addBuildCommandListener(this::sendRemoteMessage);
+        wrapper.addEndTurnCommandListener(this::sendRemoteMessage);
     }
 
     @Override
@@ -55,25 +37,16 @@ public class ClientRemoteView extends View implements Runnable {
     }
 
     @Override
-    public void run() {
-        while (true) {
-            try {
-                if (in == null)
-                    in = new ObjectInputStream(clientSocket.getInputStream());
-                Message message = (Message) in.readObject();
-                if (message instanceof BoardUpdateMessage) {
-                    onBoardUpdate((BoardUpdateMessage) message);
-                } else if (message instanceof StorageUpdateMessage) {
-                    onStorageUpdate((StorageUpdateMessage) message);
-                } else if (message instanceof NextActionsUpdateMessage) {
-                    onNextActionsUpdate((NextActionsUpdateMessage) message);
-                } else if (message instanceof TextMessage) {
-                    onText((TextMessage) message);
-                }
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-                return;
-            }
+    public void onRemoteMessage(Message message) {
+        if (message instanceof BoardUpdateMessage) {
+            onBoardUpdate((BoardUpdateMessage) message);
+        } else if (message instanceof StorageUpdateMessage) {
+            onStorageUpdate((StorageUpdateMessage) message);
+        } else if (message instanceof NextActionsUpdateMessage) {
+            onNextActionsUpdate((NextActionsUpdateMessage) message);
+        } else if (message instanceof TextMessage) {
+            onText((TextMessage) message);
         }
     }
+
 }
