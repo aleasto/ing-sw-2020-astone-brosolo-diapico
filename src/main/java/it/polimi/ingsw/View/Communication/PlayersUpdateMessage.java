1fad6f4a06c14f501ee261c40e693475990bbc5c
Alessandro Astone <ales.astone@gmail.com>
2020-05-10 18:09:07 +0200

    make the new messages thread safe
    like we did for existing messages in commit `Clonable changes`

--- src/main/java/it/polimi/ingsw/View/Communication/PlayersUpdateMessage.java
+++ src/main/java/it/polimi/ingsw/View/Communication/PlayersUpdateMessage.java
@@ -2,13 +2,22 @@ package it.polimi.ingsw.View.Communication;
 
 import it.polimi.ingsw.Game.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayersUpdateMessage extends Message {
-    List<Player> playerList;
+    private final List<Player> playerList = new ArrayList<>();
 
     public PlayersUpdateMessage(List<Player> playerList) {
-        this.playerList = playerList;
+        for (Player p : playerList) {
+            try {
+                this.playerList.add(p.clone());
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+                // Fall back to adding the actual object
+                this.playerList.add(p);
+            }
+        }
     }
 
     public List<Player> getPlayerList() {
