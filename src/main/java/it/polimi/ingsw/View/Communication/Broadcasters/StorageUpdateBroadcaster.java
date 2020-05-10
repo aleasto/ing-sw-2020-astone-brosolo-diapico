99385341906db1e18431cd1b5f22e72549576951
Alessandro Astone <ales.astone@gmail.com>
2020-05-10 21:15:08 +0200

    Better naming convention for messages
    There are two ways of sending messages:
     * The controller has knowledge of the different views it handles so it can choose
       the single view to send a message to. These classes will directly
       invoke methods on the view to send messages to it.
     * The model does not know about the views, so all it can do is to
       broadcast a message to *everyone* listening. We used to call these classes
       "dispatchers", but make them "broadcasters" as it is more clear what their
       function is!

--- src/main/java/it/polimi/ingsw/View/Communication/Broadcasters/StorageUpdateBroadcaster.java
+++ src/main/java/it/polimi/ingsw/View/Communication/Broadcasters/StorageUpdateBroadcaster.java
@@ -1,9 +1,9 @@
-package it.polimi.ingsw.View.Communication.Dispatchers;
+package it.polimi.ingsw.View.Communication.Broadcasters;
 
 import it.polimi.ingsw.View.Communication.StorageUpdateMessage;
 import it.polimi.ingsw.View.Communication.Listeners.StorageUpdateListener;
 
-public interface StorageUpdateDispatcher {
+public interface StorageUpdateBroadcaster {
     void addStorageUpdateListener(StorageUpdateListener listener);
     void removeStorageUpdateListener(StorageUpdateListener listener);
     void notifyStorageUpdate(StorageUpdateMessage message);
