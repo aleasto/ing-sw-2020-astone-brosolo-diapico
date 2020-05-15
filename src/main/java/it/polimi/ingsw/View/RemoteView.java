2618f2ab03f2f048f36b6c2dda849e97182613fa
Alessandro Astone <ales.astone@gmail.com>
2020-05-15 13:51:40 +0200

    RemoteView should handle its socket

--- src/main/java/it/polimi/ingsw/View/RemoteView.java
+++ src/main/java/it/polimi/ingsw/View/RemoteView.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.View;
 
+import it.polimi.ingsw.Exceptions.NotConnectedException;
 import it.polimi.ingsw.Game.Player;
 import it.polimi.ingsw.View.Communication.Message;
 
@@ -11,8 +12,9 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public abstract class RemoteView extends View {
-    BlockingQueue<Message> outQueue;
-    Thread networkThread;
+    protected Socket socket;
+    private BlockingQueue<Message> outQueue;
+    private Thread networkThread;
 
     public RemoteView(Player me) {
         super(me);
@@ -30,16 +32,36 @@ public abstract class RemoteView extends View {
         }
     }
 
-    public void startNetworkThread(Socket socket) {
-        networkThread = new Thread(() -> listen(socket));
+    public void startNetworkThread() {
+        networkThread = new Thread(this::listen);
         networkThread.start();
     }
 
     public void stopNetworkThread() {
         networkThread.interrupt();
+        try {
+            networkThread.join();
+        } catch (InterruptedException ignored) {}
     }
 
-    public void listen(Socket socket) {
+    public boolean isConnected() {
+        return socket != null;
+    }
+
+    public void disconnect() {
+        if (socket != null) {
+            try {
+                socket.close();
+                socket = null;
+            } catch (IOException ignored) {}
+        }
+    }
+
+    public void listen() throws NotConnectedException {
+        if (socket == null) {
+            throw new NotConnectedException("You are not connected");
+        }
+
         // A different thread for outgoing packets
         Thread outThread = new Thread(() -> {
             try {
