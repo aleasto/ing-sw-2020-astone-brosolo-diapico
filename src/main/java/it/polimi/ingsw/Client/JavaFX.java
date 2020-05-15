b2a5a133cb0768996bf3b5775bd3d2cdf1902ccd
AndreaDIapico <10619706@polimi.it>
2020-05-15 20:56:07 +0200

    Graphical Resources and Resizable Board

--- src/main/java/it/polimi/ingsw/Client/JavaFX.java
+++ src/main/java/it/polimi/ingsw/Client/JavaFX.java
@@ -1,9 +1,18 @@
 package it.polimi.ingsw.Client;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class JavaFX extends Application {
@@ -13,10 +22,14 @@ public class JavaFX extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Label l = new Label("Hello, JavaFX!");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Test");
+        BorderPane pane = new BorderPane();
+        ImageView img = new ImageView("SantoriniBoard.png");
+        img.fitWidthProperty().bind(primaryStage.widthProperty());
+        pane.setCenter(img);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
