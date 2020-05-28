c70b66ba5463851e857d117d25b449601b17960e
Alessandro Astone <ales.astone@gmail.com>
2020-05-28 23:13:15 +0200

    socket timeout with pings
     * setSoTimeout will make a read() call on the socket's input stream
       last up to the specified amount. If the timeout expires, an exception
       is thrown and we consider it disconnected and clean up.

--- src/main/java/it/polimi/ingsw/Utils/SocketInfo.java
+++ src/main/java/it/polimi/ingsw/Utils/SocketInfo.java
@@ -3,16 +3,19 @@ package it.polimi.ingsw.Utils;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Timer;
 
 public class SocketInfo {
     private final Socket socket;
     private final ObjectOutputStream out;
     private final ObjectInputStream in;
+    private final Timer pingTimer;
 
-    public SocketInfo(Socket socket, ObjectOutputStream out, ObjectInputStream in) {
+    public SocketInfo(Socket socket, ObjectOutputStream out, ObjectInputStream in, Timer pingTimer) {
         this.socket = socket;
         this.out = out;
         this.in = in;
+        this.pingTimer = pingTimer;
     }
 
     public Socket getSocket() {
@@ -25,5 +28,9 @@ public class SocketInfo {
 
     public ObjectInputStream getIn() {
         return in;
+    }
+
+    public Timer getPingTimer() {
+        return pingTimer;
     }
 }
