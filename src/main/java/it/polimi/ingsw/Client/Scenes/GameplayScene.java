f1297f8374bec1d6ac7684b82c3a387c3905c6e0
Alessandro Astone <ales.astone@gmail.com>
2020-05-30 10:16:45 +0200

    gui: major refactoring
     * Split scenes into different objects
     * Merge all gameplay related scenes into one
     * Fix god selection

---
+++ src/main/java/it/polimi/ingsw/Client/Scenes/GameplayScene.java
@@ -0,0 +1,156 @@
+package it.polimi.ingsw.Client.Scenes;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class GameplayScene extends SantoriniScene {
+
+    public static final String START_BTN = "#start_btn";
+    public static final String PLAYER_LIST = "#player_list";
+    public static final String PLAYER_LIST_PANE = "#player_list_pane";
+    public static final String SELECT_GODS_BTN = "#select_gods_btn";
+    public static final String GOD_LIST = "#god_list";
+    public static final String GOD_SELECTION_VIEW = "#god_selection_view";
+    public static final String TRANSPARENCY = "#transparency";
+    public static final String BOARD = "#board";
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    double width = screenSize.getWidth();
+    double height = screenSize.getHeight();
+
+    private final Scene scene;
+
+    public GameplayScene() {
+        Image background = new Image("SantoriniBoard.png", width, height, true, true);
+
+        // Stack up different panes (background, board, godpool, etc...)
+        StackPane stack = new StackPane(new ImageView(background));
+
+        // Transparency
+        Rectangle transparency = new Rectangle(width, height);
+        transparency.setFill(Color.rgb(0, 0, 0, 0.90));
+        transparency.setId(SET_ID(TRANSPARENCY));
+        stack.getChildren().add(transparency);
+
+        // Online players
+        VBox playerListContainer = new VBox(10);
+        playerListContainer.setId(SET_ID(PLAYER_LIST));
+        playerListContainer.setAlignment(Pos.CENTER);
+        Rectangle onlinePlayersBG = new Rectangle(250, height / 2, Color.ORANGE);
+        AnchorPane playersAnchorPane = new AnchorPane(onlinePlayersBG, playerListContainer);
+        AnchorPane.setTopAnchor(playerListContainer, 15d);
+        AnchorPane.setRightAnchor(playerListContainer, 120d);
+        AnchorPane.setTopAnchor(onlinePlayersBG, 1d);
+        AnchorPane.setRightAnchor(onlinePlayersBG, 1d);
+        playersAnchorPane.setId(SET_ID(PLAYER_LIST_PANE));
+        stack.getChildren().add(playersAnchorPane);
+
+        // Start button
+        Button startTheGame = new Button("Start!");
+        startTheGame.setVisible(false); // only the host will show it
+        startTheGame.setId(SET_ID(START_BTN));
+        stack.getChildren().add(startTheGame);
+
+        // God selection
+        Button selectGodsBtn = new Button("Select");
+        selectGodsBtn.setDisable(true);
+        selectGodsBtn.setId(SET_ID(SELECT_GODS_BTN));
+        HBox godsPlayable = new HBox(20);
+        godsPlayable.setAlignment(Pos.CENTER);
+        godsPlayable.setId(SET_ID(GOD_LIST));
+        VBox godSelectionView = new VBox(2);
+        godSelectionView.getChildren().addAll(godsPlayable, selectGodsBtn);
+        godSelectionView.setAlignment(Pos.CENTER);
+        godSelectionView.setSpacing(45);
+        godSelectionView.setId(SET_ID(GOD_SELECTION_VIEW));
+        godSelectionView.setVisible(false);
+        stack.getChildren().add(godSelectionView);
+
+        // Board grid
+        GridPane boardGrid = new GridPane();
+        boardGrid.setAlignment(Pos.CENTER);
+        boardGrid.setPadding(new Insets(15, 0, 0, 0));
+        boardGrid.setHgap(13);
+        boardGrid.setVgap(13);
+        //TODO: Make dimensions settable maybe
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Rectangle rectangle = new Rectangle(height/7.5d, height/7.5d);
+                rectangle.setFill(Color.BLACK);
+                GridPane.setRowIndex(rectangle, i);
+                GridPane.setColumnIndex(rectangle, j);
+                boardGrid.getChildren().add(rectangle);
+            }
+        }
+        boardGrid.setId(SET_ID(BOARD));
+        boardGrid.setVisible(false);
+        stack.getChildren().add(boardGrid);
+
+        this.scene = new Scene(stack, width, height);
+    }
+
+    public void showAndPickGods(List<String> gods, boolean shouldPick, int howMany, Consumer<List<String>> selectAction) {
+        HBox godsPlayable = lookup(GOD_LIST);
+        Button selectGodsBtn = lookup(SELECT_GODS_BTN);
+        List<String> chosenGods = new ArrayList<>();
+
+        selectGodsBtn.setVisible(shouldPick);
+        selectGodsBtn.setDisable(true);
+
+        godsPlayable.getChildren().clear();
+        for (String god : gods) {
+            //Create the god image
+            ImageView imageView = new ImageView(new Image("/godcards/" + god + ".png", width / 15, height * 0.4, true, true));
+
+            if (shouldPick) {
+                //Set click functionality on the images
+                imageView.setPickOnBounds(true);
+                imageView.setOnMouseClicked(e -> {
+                    if (e.getButton() == MouseButton.PRIMARY) {
+                        if(chosenGods.size() < howMany && !chosenGods.contains(god)) {
+                            //Left click to select
+                            Glow glow = new Glow(0.7);
+                            imageView.setEffect(glow);
+                            chosenGods.add(god);
+                        }
+                    } else if (e.getButton() == MouseButton.SECONDARY) {
+                        //Right click to deselect
+                        imageView.setEffect(null);
+                        chosenGods.remove(god);
+                    }
+
+                    selectGodsBtn.setDisable(chosenGods.size() != howMany);
+                });
+            }
+            godsPlayable.getChildren().add(imageView);
+        }
+
+        selectGodsBtn.setOnAction(e -> {
+            if (chosenGods.size() == howMany) {
+                selectAction.accept(chosenGods);
+            }
+        });
+        this.<Node>lookup(GOD_SELECTION_VIEW).setVisible(true);
+    }
+
+    @Override
+    public Scene getFXScene() {
+        return scene;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Client/Scenes/LoginScene.java
@@ -0,0 +1,81 @@
+package it.polimi.ingsw.Client.Scenes;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
+
+public class LoginScene extends SantoriniScene {
+
+    public static final String NAME_INPUT = "#name_input";
+    public static final String LVL_INPUT = "#lvl_input";
+    public static final String LOGIN_BTN = "#login_btn";
+
+    private Scene scene;
+
+    public LoginScene() {
+        Image image = new Image("santorini.jpg");
+        BackgroundFill bg = new BackgroundFill(new ImagePattern(image), CornerRadii.EMPTY, Insets.EMPTY);
+
+        //Labels and text fields
+        Label loginPrompt = new Label("Please input your username and godlikelvl");
+        loginPrompt.setTextFill(Color.BLACK);
+        loginPrompt.setFont(Font.font(loginPrompt.getFont().toString(), FontWeight.BOLD, 15));
+
+        TextField nameInput = new TextField();
+        nameInput.setPromptText("Username");
+        nameInput.setId(SET_ID(NAME_INPUT));
+
+        TextField lvlInput = new TextField();
+        lvlInput.setPromptText("Godlike level");
+        lvlInput.setId(SET_ID(LVL_INPUT));
+
+        //Set the textfield to accept only numbers
+        DecimalFormat format = new DecimalFormat("#");
+        lvlInput.setTextFormatter(new TextFormatter<>(c -> {
+            if (c.getControlNewText().isEmpty()) {
+                return c;
+            }
+            ParsePosition parsePosition = new ParsePosition(0);
+            Object object = format.parse(c.getControlNewText(), parsePosition);
+
+            if (object == null || parsePosition.getIndex() < c.getControlNewText().length())
+                return null;
+            else return c;
+        }));
+
+        Button enterBtn = new Button("Enter");
+        enterBtn.setId(SET_ID(LOGIN_BTN));
+
+        //Layout declaration
+        GridPane grid = new GridPane();
+        VBox verticalLayout = new VBox(20);
+        HBox inputBox = new HBox(20);
+
+        //Add components to the layouts and set the scene
+        inputBox.getChildren().addAll(nameInput, lvlInput);
+        verticalLayout.getChildren().addAll(loginPrompt, inputBox, enterBtn);
+        grid.setAlignment(Pos.CENTER);
+        grid.add(verticalLayout, 0, 0);
+        grid.setBackground(new Background(bg));
+
+        this.scene = new Scene(grid, 800, 600);
+    }
+
+    @Override
+    public Scene getFXScene() {
+        return scene;
+    }
+}
