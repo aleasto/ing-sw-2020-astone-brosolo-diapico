c51350b9eec0911e3255da6c33d1017f9565a099
Alessandro Astone <ales.astone@gmail.com>
2020-05-30 10:42:37 +0200

    gui: only enable endturn button when i can finish my turn

--- src/main/java/it/polimi/ingsw/View/Communication/NextActionsUpdateMessage.java
+++ src/main/java/it/polimi/ingsw/View/Communication/NextActionsUpdateMessage.java
@@ -5,10 +5,14 @@ import java.util.List;
 public class NextActionsUpdateMessage extends Message {
     private final List<MoveCommandMessage> nextMoves;
     private final List<BuildCommandMessage> nextBuilds;
+    private final boolean mustMove;
+    private final boolean mustBuild;
 
-    public NextActionsUpdateMessage(List<MoveCommandMessage> nextMoves, List<BuildCommandMessage> nextBuilds) {
+    public NextActionsUpdateMessage(List<MoveCommandMessage> nextMoves, List<BuildCommandMessage> nextBuilds, boolean mustMove, boolean mustBuild) {
         this.nextMoves = nextMoves;
         this.nextBuilds = nextBuilds;
+        this.mustMove = mustMove;
+        this.mustBuild = mustBuild;
     }
 
     public List<MoveCommandMessage> getNextMoves() {
@@ -17,5 +21,13 @@ public class NextActionsUpdateMessage extends Message {
 
     public List<BuildCommandMessage> getNextBuilds() {
         return nextBuilds;
+    }
+
+    public boolean mustMove() {
+        return mustMove;
+    }
+
+    public boolean mustBuild() {
+        return mustBuild;
     }
 }
