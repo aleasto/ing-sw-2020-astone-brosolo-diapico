ca591cfe803856d2a3629f4a364890703ceb2696
Alessandro Astone <ales.astone@gmail.com>
2020-06-16 22:09:51 +0200

    add utils javadoc

--- src/main/java/it/polimi/ingsw/Utils/Log.java
+++ src/main/java/it/polimi/ingsw/Utils/Log.java
@@ -3,10 +3,23 @@ package it.polimi.ingsw.Utils;
 import it.polimi.ingsw.Game.Player;
 
 public class Log {
+    /**
+     * Print a nicely formatted log for a player action
+     *
+     * @param p the player this log is associated with
+     * @param log a string message representing the action
+     */
     public static void logPlayerAction(Player p, String log) {
         System.out.println(" * " + p.toString() + " " + log);
     }
 
+    /**
+     * Print a nicely formatted log for a failed player action
+     *
+     * @param p the player this log is associated with
+     * @param action the action that failed
+     * @param reason the reason for the failure
+     */
     public static void logInvalidAction(Player p, String action, String reason) {
         logPlayerAction(p, "issued an invalid `" + action + "` command: " + (reason != null ? reason : ""));
     }
