32c4f27a5af767bfab0f7c3c01cb8cc888aa43dd
Alessandro Astone <ales.astone@gmail.com>
2020-06-25 16:24:35 +0200

    add some forgotten view javadoc

--- src/main/java/it/polimi/ingsw/View/ClientRemoteView.java
+++ src/main/java/it/polimi/ingsw/View/ClientRemoteView.java
@@ -12,6 +12,11 @@ public abstract class ClientRemoteView extends RemoteView {
         super(me);
     }
 
+    /**
+     * Receives a message event from the network, and invokes its delegate method according to its instance type.
+     *
+     * @param message the message object
+     */
     @Override
     public void onRemoteMessage(Message message) {
         if (message instanceof BoardUpdateMessage) {
@@ -39,16 +44,36 @@ public abstract class ClientRemoteView extends RemoteView {
         }
     }
 
+    /**
+     * Pipes all commands to the network.
+     * The receiver, if listening, will receive an `onCommand` event
+     *
+     * @param message the command
+     */
     @Override
     public void onCommand(CommandMessage message) {
         // Forward command to the network
         sendRemoteMessage(message);
     }
 
+
+    /**
+     * Connect this view to a tcp server.
+     *
+     * @param ip the host
+     * @param port the port
+     * @throws IOException if we fail to open the socket
+     */
     public void connect(String ip, int port) throws IOException {
         this.socket = new Socket(ip, port);
     }
 
+    /**
+     * Join a lobby on the server.
+     * This sends a command just like onCommand() would do, but we'll keep it as a separate method cause it's fancier.
+     *
+     * @param lobby the lobby name
+     */
     public void join(String lobby) {
         sendRemoteMessage(new JoinCommandMessage(getPlayer(), lobby));
     }
