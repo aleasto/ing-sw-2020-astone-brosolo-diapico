aleasto/ing-sw-2020-astone-brosolo-diapico

commit b9c2100524414cc56e2f11a67fbe3a10d7265497

gui: fix choosing which block to build

src/main/java/it/polimi/ingsw/Client/JavaFX/GUI.java
@@ -565,7 +565,7 @@ public class GUI extends Application {
                 buildBox.setVisible(true);
                 gameplayScene.<Node>lookup(GameplayScene.ACTIONS_BOX).setVisible(false);
             } else {
-                doBuild(x, y, board.getAt(x, y).getHeight());
+                doBuild(x, y, availBuilds.get(0).getBlock());
             }
         }
 

commit e29a35f9ca4ade662e3cccf077a2ab198d5d2595

gui: don't crash when trying to build on an invalid spot
 * this was a regression introduced in b9c2100

src/main/java/it/polimi/ingsw/Client/JavaFX/GUI.java
@@ -568,8 +568,11 @@ public class GUI extends Application {
                 buildBox.setTranslateY(tileNode.getLayoutY() + boardNode.getLayoutY() - buildBox.getLayoutY());
                 buildBox.setVisible(true);
                 gameplayScene.<Node>lookup(GameplayScene.ACTIONS_BOX).setVisible(false);
-            } else {
+            } else if (availBuilds.size() == 1) {
                 doBuild(x, y, availBuilds.get(0).getBlock());
+            } else {
+                // Let's poke the server anyways, even if we know we cannot build there
+                doBuild(x, y, board.getAt(x, y).getHeight());
             }
         }
 